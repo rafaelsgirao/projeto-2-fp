7cbba1564701f922aa9dad42c94c708925cf85eb
Rafael <7544832+rafaelsgirao@users.noreply.github.com>
2021-01-02 17:13:31 +0000

    Ah yes, another commit
    s

--- 2ndo projeto.py
+++ 2ndo projeto.py
@@ -1,10 +1,19 @@
-
 debug = True
+
+def debug_vars():
+    global debug
+    if debug:
+        global pos1, pos2, t
+
+        pos1 = cria_posicao("b", "2")
+        pos2 = cria_posicao("b", "3")
+        t = cria_tabuleiro()
 
 def debug_msgs(input):
     global debug
     if debug:
         print(input)
+
 #ist199309 Rafael Girao - Projeto 2 de FP
 
 #------------------------TAD posicao (1.5 valores)------------------
@@ -12,9 +21,9 @@ def debug_msgs(input):
 #Construtores
 
 def cria_posicao(c, l):
+
     cols = ("a","b","c")
     linhas = ("1","2","3")
-
     if not(c in cols and l in linhas):
         raise ValueError("cria_posicao: argumentos invalidos")
     
@@ -46,24 +55,28 @@ def posicoes_iguais(p1, p2):
 def posicao_para_str(p):
     return p["c"] + p["l"]
 
+#Funcao auxiliar: Retorna todas as posicoes existentes
+
+def gera_todas_posicoes():
+    cols = ("a","b","c")
+    linhas = ("1","2","3")
+    return [cria_posicao(c,l) for l in linhas for c in cols]
+    
 #Funcao de alto nivel
 
-#ACABAR ESTA FUNCAO
+#Done, i guess
 def obter_posicoes_adjacentes(p):
 
     c, l = obter_pos_c(p), obter_pos_l(p)
-
-    cols = ("a", "b", "c")
-    linhas = ("1","2","3")
     
-
     centro = cria_posicao("b", "2")
+    posicoes = gera_todas_posicoes()
 
     #Caso de ser o centro
     if posicoes_iguais(p, centro):
-        rslt = [cria_posicao(c,l) for c in cols for l in linhas]
-        rslt.remove(centro)
-        return rslt
+        posicoes.remove(centro)
+        return tuple(posicoes)
+    #Todos os outros casos
     else:
         rslt = []
         if c == "a" or c == "c":
@@ -80,7 +93,14 @@ def obter_posicoes_adjacentes(p):
         if c != "b" and l != "2":
             debug_msgs("c = {} and l = {}".format(c, l))
             rslt.append(centro)
-    return rslt
+
+        #Ordenar o resultado (rslt_ordenado = resultado ordenado)
+        rslt_ordenado = ()
+        for pos in posicoes:
+            if pos in rslt:
+                rslt_ordenado = rslt_ordenado + (pos,)
+
+    return rslt_ordenado
    
 
 
@@ -103,8 +123,10 @@ def cria_copia_peca(p):
 
 #Reconhecedores
 def eh_peca(p):
-    return isinstance(p, dict) and p["p"] in (-1, 0, 1) and \
-        len(p1) == 1
+    if "p" not in p:
+        return False
+    return isinstance(p, dict) and p["p"] in (-1, 0, 1) \
+        and len(p) == 1
 
 #Teste
 
@@ -139,11 +161,9 @@ def peca_para_inteiro(p):
 #Construtores
 def cria_tabuleiro():
     t = {}
-    cols, linhas = ("a","b","c"), ("1","2","3")
-    posicoes = [cria_posicao(c,l) for c in cols for l in linhas]
     peca_vazia = cria_peca(" ")
-    for pos in posicoes:
-        t[pos] = peca_vazia
+    for pos in gera_todas_posicoes():
+        t[posicao_para_str(pos)] = peca_vazia
     return t
 
 def cria_copia_tabuleiro(t):
@@ -152,61 +172,67 @@ def cria_copia_tabuleiro(t):
 #Seletores
 
 def obter_peca(t, pos):
-    return t[pos-1]
+    return t[posicao_para_str(pos)]
 
 def obter_vetor(t, vect):
-    #Caso do vetor pedido ser de uma linha
-    i = False
-    if vect == "a":
-        i = 0
-    if vect == "b":
-        i = 1
-    if vect == "c":
-        i = 2
-    if i is not False:
-        return [t[i], t[i+3], t[i+6]]
-    if vect == "1":
-        i = 0
-    elif vect == "2":
-        i = 3
-    elif vect == "3":
-        i = 6
-    return [t[i], t[i+1], t[i+2]]
 
+    rslt = ()
+    cols = ("a","b","c")
+    linhas = ("1", "2", "3")
+    #Caso do vetor pedido ser coluna
+    if vect in cols:
+        #vect_posicoes = Vetor com as posicoes do vetor pedido)
+        vect_posicoes = [cria_posicao(vect, l) for l in linhas]
+        debug_msgs("obter_vetor | vect_posicoes = {}".format(vect_posicoes))
+    #Caso do vetor pedido ser linha
+    else:
+        vect_posicoes = [cria_posicao(l, vect) for c in cols]
+        debug_msgs("obter_vetor | vect_posicoes = {}".format(vect_posicoes))
+    
+    for pos in vect_posicoes:
+        rslt = rslt + (t[posicao_para_str(pos)],)
+    return rslt
 #Modificadores
 def coloca_peca(tab, peca, pos):
-    tab[pos-1] = peca
+    tab[posicao_para_str(pos)] = peca
     return tab
 
 def remove_peca(tab, pos):
-    tab[pos-1] = cria_peca(" ")
+    tab[posicao_para_str(pos)] = cria_peca(" ")
     return tab
 
 def move_peca(tab, pos1, pos2):
-    tab[pos2-1] = tab[pos1-1]
-    tab[pos1-1] = cria_peca("")
+    str_pos1, str_pos2 = posicao_para_str(pos1), posicao_para_str(pos2)
+    tab[str_pos2] = tab[str_pos1]
+    tab[str_pos1] = cria_peca("")
     return tab
 
 #Reconhecedores
 
 def eh_tabuleiro(t):
-    #Verificar se t eh lista de 9 elementos
-    if not isinstance(t, list) and len(t) == 9: return False
-
+    #Verificar se t eh dicionario de 9 elementos
+    if not isinstance(t, dict) and len(t) == 9:
+        return False
+    posicoes = gera_todas_posicoes()
     count_x, count_o = 0,0
-    for peca in t:
-        #repr_peca = Representacao externa da peca
-        repr_peca = peca_para_str(peca)
 
-        if not eh_peca(peca): 
+    for pos in posicoes:
+        str_pos = posicao_para_str(pos)
+        peca = t[str_pos]
+        if not(str_pos in t and eh_peca(peca)):
+            debug_msgs("eh_tabuleiro | str_pos in t: {}".format(str(str_pos in t)))
+            debug_msgs("eh_tabuleiro | eh_peca(peca): {}".format(str(eh_peca(peca))))
+            debug_msgs("eh_tabuleiro | peca = : {}".format(peca))
             return False
-        elif repr_peca == "[X]":
-            count_x += 1
-        elif repr_peca == "[O]":
-            count_o += 1
-        #Testar se o tab tem 2 ou mais pecas que o adversario
-        if abs(count_x - count_o) >= 2:
+        else:
+            str_peca = peca_para_str(peca)
+            if str_peca == "[X]":
+                count_x += 1
+            elif str_peca == "[O]":
+                count_o += 1
+        if abs(count_o - count_x) >= 2:
             return False
+
 
     return True
 
@@ -223,30 +249,38 @@ def tabuleiro_para_str(t):
     | / | \ |
     3 {}-{}-{}
     """
-
-    pecas = []
-    for i in range(1,10):
-        pecas.append(peca_para_str(obter_peca(t , i)))
-    tabstr = tabstr.format(pecas)
+    #return tabstr
+    posicoes = gera_todas_posicoes()
+    pecas_ordenadas = []
+    for pos in posicoes:
+        pecas_ordenadas.append(peca_para_str(obter_peca(t, pos)))
+    pecas_ordenadas = tuple(pecas_ordenadas)
+    debug_msgs("tabuleiro_para_str | pecas_ordenadas = {}".format(pecas_ordenadas))
+    tabstr = tabstr.format(*pecas_ordenadas)
     return tabstr
 
+
+    
 def tuplo_para_tabuleiro(tpl):
     t = cria_tabuleiro()
-    
-    i = 1
-    for linha in tpl:
-        for p in linha:
-            if p == -1:
-                nova_p = cria_peca("O")
-                
-            elif p == 0:
-                nova_p = cria_peca(" ")
-            elif p == 1:
-                nova_p = cria_peca("X")
-            coloca_peca(t, nova_p, i)
+    #Achatar o tuplo recebido
+    tpl = [valor for linha in tpl for valor in linha]
+
+    i = 0
+    posicoes = gera_todas_posicoes()
+    for valor in tpl:
+        if valor == -1:
+            peca = cria_peca("O")
+        elif valor == 1:
+            peca = cria_peca("X")
+        else:
+            peca = cria_peca(" ")
+        coloca_peca(t, peca, posicoes[i])
+
+        i+=1
+    return t
+
 
 
 #delete later
-p1 = cria_posicao("b", "2")
-
-p2 = cria_posicao("b", "3")
+debug_vars()
